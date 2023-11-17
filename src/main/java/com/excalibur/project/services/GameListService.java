b98f538ca8fb7a9b95b00621ad703bf3183e07a4
Arthur Queiroz <arthutqueiroz@hotmail.com>
2023-11-16 23:06:47 -0300

    Move games on list

--- src/main/java/com/excalibur/project/services/GameListService.java
+++ src/main/java/com/excalibur/project/services/GameListService.java
@@ -9,6 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.excalibur.project.dto.GameListDTO;
 import com.excalibur.project.entities.GameList;
+import com.excalibur.project.projections.GameMinProjection;
+import com.excalibur.project.repositories.GameRepository;
 import com.excalibur.project.repositories.GameListRepository;
 
 @Service
@@ -17,9 +19,28 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(GameListDTO::new).toList();
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 }
